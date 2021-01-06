e35c11bf80dbf31d7fc5e25ee0d74ad0bb19b2b1
Kelvin <kwardeu@gmail.com>
2021-01-06 15:43:20 +0000

    MINOR: Profiles are created when user is created

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -14,3 +16,15 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile through signals/reciever
+    Needs post_save and reviever importing
+    """
+    print('##### in signals')
+    if created:
+        UserProfile.objects.create(user=instance)
+    # If user exists just save the profile
+    instance.userprofile.save()
