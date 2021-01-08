b5bbb998943c3808687d378f5fe3730c3bf1cb88
Kelvin <kwardeu@gmail.com>
2021-01-08 11:03:35 +0000

    BUGFIX: Unauthenticated user would cause error viewing lessons in Studio app

--- studio/views.py
+++ studio/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from lessons.models import Lesson
 from profiles.models import UserProfile
 
+from yoga.utils import get_profile_or_none
+
 def studio(request, id):
     """ A view for the studio """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    profile = get_profile_or_none(request)
     lesson = get_object_or_404(Lesson, lesson_id=id)
 
     template = "studio/studio.html"
