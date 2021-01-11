8b266d96acd7824fb2c041c64511fd047f2655f2
Kelvin <kwardeu@gmail.com>
2021-01-11 18:35:54 +0000

    FEAT: Home button is context sensitive FindInstructor/MyLessons

--- home/views.py
+++ home/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from lessons.models import LessonItem
+
+from yoga.utils import get_profile_or_none
 
 def index(request):
     """ A view to return Home page """
-    template = "home/index.html"
+    profile = get_profile_or_none(request)
+    subscribed_lessons = False
 
-    return render(request, template)
+    if profile:
+        if LessonItem.objects.filter(user=profile).count() > 0:
+            subscribed_lessons = True
+
+    template = "home/index.html"
+    context = {
+        'profile': profile,
+        'subscribed_lessons': subscribed_lessons,
+    }
+
+    return render(request, template, context)
