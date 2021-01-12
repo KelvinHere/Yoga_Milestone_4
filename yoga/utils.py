a9a2d8e6f9229065d93fb902ffa42ed39a97394e
Kelvin <kwardeu@gmail.com>
2021-01-12 19:28:01 +0000

    FEAT: yoga.utils.py PIL image resize function start

--- yoga/utils.py
+++ yoga/utils.py
@@ -1,4 +1,5 @@
 from profiles.models import UserProfile
+from PIL import Image
 
 """
 Utility functions to be used by entire site
@@ -10,3 +11,14 @@ def get_profile_or_none(request):
         return UserProfile.objects.get(user=request.user)
     except:
         return None
+
+def size_profile_image(input_image):
+    print('im in!')
+    im = Image.open(input_image)
+    if im.height < 400:
+        print(">")
+    else: 
+        print("<")
+
+
+size_profile_image('media/profile_images/instructor_2.jpg')
