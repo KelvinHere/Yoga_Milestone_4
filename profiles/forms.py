1bbf7971e5c40b273c29387b329bb1ef643fe00f
Kelvin <kwardeu@gmail.com>
2021-01-13 12:55:27 +0000

    MINOR: Instructor card description only appears in form if the profile is an instructor

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,14 +9,20 @@ class ProfileForm(forms.ModelForm):
         model = UserProfile
         fields = '__all__'
         widgets = {
-                   'user': forms.HiddenInput,
-                   'is_instructor': forms.HiddenInput,
-                   'rating': forms.HiddenInput,
-                  }
+            'user': forms.HiddenInput,
+            'is_instructor': forms.HiddenInput,
+            'rating': forms.HiddenInput,
+        }
+        labels = {
+            'card_description': 'Instructor card description'
+        }
 
     # Over-ride init
     def __init__(self, *args, **kwargs): 
         super(ProfileForm, self).__init__(*args, **kwargs)  
+
+        card_desctiption_placeholder = 'A brief description of yourself and ethos (256 characters)'
         self.fields['user'].disabled = True
         self.fields['is_instructor'].disabled = True
         self.fields['rating'].disabled = True
+        self.fields['card_description'].widget = forms.Textarea(attrs={'rows': 3, 'cols': 25, 'maxlength': 254, 'placeholder': card_desctiption_placeholder})
