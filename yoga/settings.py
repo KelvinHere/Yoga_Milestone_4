2fe9840ab70b0f98119fde71c519854cd88f8dcd
Kelvin <kwardeu@gmail.com>
2021-01-14 17:28:42 +0000

    BUGFIX: Settings.py wrong setting in database

--- yoga/settings.py
+++ yoga/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'yoga.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get(DATABASE_URL))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
 else:
     DATABASES = {
