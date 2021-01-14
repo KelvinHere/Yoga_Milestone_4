9d3f5813f7ad2a46878446d2e8fa594cdce1c8b5
Kelvin <kwardeu@gmail.com>
2021-01-14 22:18:01 +0000

    BUGFIX: s3 changed variable names again

--- yoga/settings.py
+++ yoga/settings.py
@@ -196,8 +196,8 @@ if 'USE_BACKBLAZE' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{BB_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{BB_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 #Crispy forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
