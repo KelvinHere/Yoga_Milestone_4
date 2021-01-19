ab6d1a7cb637e87f71289d8801b006d0a8dae3b4
Kelvin <kwardeu@gmail.com>
2021-01-19 13:09:25 +0000

    FEAT: Write Review button becomes Edit Review if review already exists

--- studio/views.py
+++ studio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from lessons.models import Lesson
+from lessons.models import Lesson, LessonReview
 from profiles.models import UserProfile
 
 from yoga.utils import get_profile_or_none
@@ -8,11 +8,13 @@ def studio(request, id):
     """ A view for the studio """
     profile = get_profile_or_none(request)
     lesson = get_object_or_404(Lesson, lesson_id=id)
+    existing_review = LessonReview.objects.filter(profile=profile, lesson=lesson).first()
 
     template = "studio/studio.html"
     context = {
         'profile': profile,
         'lesson': lesson,
+        'existing_review': existing_review,
     }
 
     return render(request, template, context)
