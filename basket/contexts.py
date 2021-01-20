77109c237d13c3501ab6d74003dccfddfa25fac5
Kelvin <kwardeu@gmail.com>
2021-01-20 10:30:23 +0000

    FEAT: Basket items show in basket.html with totals, discounts and percentages

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,18 +1,33 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from lessons.models import Lesson
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for lesson_id in basket:
+        lesson = get_object_or_404(Lesson, lesson_id=lesson_id)
+        total += lesson.price
+        product_count += 1
+        print(lesson)
+        basket_items.append({
+            'lesson_id': lesson.lesson_id,
+            'lesson_name': lesson.lesson_name,
+            'price': lesson.price,
+        })
+
 
     if total >= settings.DISCOUNT_THRESHOLD:
         discount = settings.DISCOUNT_PERCENTAGE * Decimal(total / 100)
-        discount_delta = settings.DISCOUNT_THRESHOLD - total
     else:
         discount = 0
-        discount_delta = 0
+        discount_delta = settings.DISCOUNT_THRESHOLD - total
 
     grand_total = total - discount
 
@@ -22,6 +37,7 @@ def basket_contents(request):
         'product_count': product_count,
         'discount': discount,
         'discount_delta': discount_delta,
+        'discount_percentage': settings.DISCOUNT_PERCENTAGE,
         'grand_total': grand_total,
     }
 
