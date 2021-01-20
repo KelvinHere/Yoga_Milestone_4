f777d027b54ba3d3c232d2f4e1458f4d6cd48866
Kelvin <kwardeu@gmail.com>
2021-01-20 10:03:12 +0000

    FEAT: Wire buy button to add lesson to session in add_to_basket view

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
+    path('add_to_basket/<lesson_id>/', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 def view_basket(request):
     """ A view to view the basket """
 
-    return render(request, 'basket/basket.html')
+    template = 'basket/basket.html'
+
+    return render(request, template)
+
+def add_to_basket(request, lesson_id):
+    """ Add a lesson to the basket """
+
+    # Create session for basket so user can browse site without losing basket contents
+    basket = request.session.get('basket', {})
+
+    if lesson_id not in list(basket.keys()):
+        basket[lesson_id] = 1
+
+    request.session['basket'] = basket
+
+    template = "home/index.html"
+    context = {
+    }
+
+    return render(request, template, context)
