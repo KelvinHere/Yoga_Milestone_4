5d7edaea951f58c280954f87093daf06f1776a83
Kelvin <kwardeu@gmail.com>
2021-01-21 20:02:41 +0000

    FEAT: Lesson can only be accessed if you own it or it is free

--- studio/views.py
+++ studio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 
 from lessons.models import Lesson, LessonReview
 from checkout.models import OrderLineItem
@@ -12,6 +12,7 @@ def studio(request, id):
     lesson = get_object_or_404(Lesson, lesson_id=id)
     existing_user_review = LessonReview.objects.filter(profile=profile, lesson=lesson).first()
     lesson_reviews = LessonReview.objects.filter(lesson=lesson)
+    paid_lessons = OrderLineItem.objects.filter(profile=profile)
 
     template = "studio/studio.html"
     context = {
@@ -21,4 +22,8 @@ def studio(request, id):
         'lesson_reviews': lesson_reviews,
     }
 
-    return render(request, template, context)
+    if paid_lessons.filter(lesson=lesson) or lesson.is_free:
+        print('###paid one here')
+        return render(request, template, context)
+    else:
+        return HttpResponse('You do not own this lesson', status=500)
