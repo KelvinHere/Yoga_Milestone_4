cf25e287982ed91adf31fc93b16ae95e84679d91
Kelvin <kwardeu@gmail.com>
2021-01-21 11:33:17 +0000

    FEAT: checkout.html and checkout form styled

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,17 +6,11 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email')
+        labels = {
+            'full_name': 'Full Name ',
+            'email': 'Email Address '
+        }
 
     def __init__(self, *args, **kwargs):
         """ Format form """
         super().__init__(*args, **kwargs)
-
-        placeholders = {
-            'full_name': 'Full Name*',
-            'email': 'Email Address*'
-        }
-
-        for field in self.fields:
-            placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholders'] = placeholder
-            self.fields[field].label = False
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-def checkout(self):
-    return HttpResponse(status=200)
+from .forms import OrderForm
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "Your basket is empty")
+        return redirect(reverse('index'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
