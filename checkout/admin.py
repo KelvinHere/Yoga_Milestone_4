668b6fdf9a4e59e3f46f6cc9f143760c9ec3412f
Kelvin <kwardeu@gmail.com>
2021-01-21 10:33:14 +0000

    FEAT: Create order admin and signals for lineitem update/create/delete

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date', 'discount',
+                       'order_total', 'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name', 'email',
+              'order_total', 'discount', 'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
