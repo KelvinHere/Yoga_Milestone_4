6aea78720be51e604c02245af6599db11a682c56
Kelvin <kwardeu@gmail.com>
2021-01-21 12:40:09 +0000

    BUGFIX: Card element style not applied in JS properly

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,7 +2,6 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var card = elements.create('card');
 var style = {
     base: {
         color: '#534a47',
@@ -18,4 +17,5 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
