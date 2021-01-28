35caf958cc9796ba8027b87ed121747ab4d17073
Kelvin <kwardeu@gmail.com>
2021-01-28 14:53:50 +0000

    MINOR: format lessons.views

--- lessons/views.py
+++ lessons/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpR
 from django.db.models import Q, F
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile, User
+from .models import UserProfile
 from lessons.models import Lesson, LessonItem, LessonReview
 from checkout.models import OrderLineItem
 import json
@@ -34,7 +34,8 @@ def lessons(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             if sortkey not in valid_sort_values:
-                messages.error(request, 'Invalid sort value, displaying all lessons by name in ascending order')
+                messages.error(request, 'Invalid sort value, displaying all \
+                                         lessons by name in ascending order')
                 return redirect(reverse('lessons'))
 
             if sortkey == 'name':
@@ -63,19 +64,27 @@ def lessons(request):
             if request.GET['instructor']:
                 instructor_id = request.GET['instructor']
                 try:
-                    instructor_to_display = get_object_or_404(UserProfile, id=instructor_id)
+                    instructor_to_display = get_object_or_404(UserProfile,
+                                                              id=instructor_id)
                     if not instructor_to_display.is_instructor:
-                        messages.error(request, 'This user is not an instructor, please pick one from the instructor list.')
+                        messages.error(request,
+                                       'This user is not an instructor, \
+                                       please pick one from the \
+                                       instructor list.')
                         return redirect(reverse('instructors'))
                 except Exception as e:
-                    messages.error(request, 'This instructor was not found, please pick one from the instructor list.')
+                    messages.error(request,
+                                   'This instructor was not found, please \
+                                   pick one from the instructor list.')
                     return redirect(reverse('instructors'))
                 page_title = f"Welcome to {instructor_to_display}'s Studio"
-                lessons = lessons.filter(instructor_profile=instructor_to_display)
+                lessons = lessons.filter(
+                    instructor_profile=instructor_to_display
+                    )
 
     # If authenticated
     if request.user.is_authenticated:
-    
+
         # Get a list of subscribed lesson IDs for current user
         subscribed_lessons = LessonItem.objects.filter(user=profile)
         for subscribed_lesson in subscribed_lessons:
@@ -106,7 +115,7 @@ def lessons(request):
                 page_title = 'Purchased Lessons'
                 sub_title = 'You have not purchased any lessons'
             filter_title = 'Purchaed Lessons'
-        
+
         # If viewing an instructor and also filtering
         if instructor_to_display:
             page_title = f"Welcome to {instructor_to_display}'s Studio"
@@ -148,23 +157,28 @@ def subscriptions(request):
             lesson_id = request.GET['lesson_id']
             lesson_object = Lesson.objects.get(lesson_id=lesson_id)
         except Exception as e:
-            messages.error(request, 'Invalid request, no lessons have been subscribed or unsubscribed to.')
+            messages.error(request,
+                           'Invalid request, no lessons have been subscribed \
+                           or unsubscribed to.')
             return redirect(reverse('lessons'))
 
         if request.GET['subscribe'] == 'false':
-            unsubscribe = LessonItem.objects.filter(lesson=lesson_object, user=profile)
+            unsubscribe = LessonItem.objects.filter(lesson=lesson_object,
+                                                    user=profile)
             unsubscribe.delete()
             json_response = json.dumps({'subscription_status': 'unsubscribed'})
             return HttpResponse(json_response, content_type='application/json')
 
         elif request.GET['subscribe'] == 'true':
-            if not LessonItem.objects.filter(lesson=lesson_object, user=profile).exists():
+            if not LessonItem.objects.filter(lesson=lesson_object,
+                                             user=profile).exists():
                 LessonItem.objects.create(lesson=lesson_object, user=profile)
             json_response = json.dumps({'subscription_status': 'subscribed'})
             return HttpResponse(json_response, content_type='application/json')
 
         else:
-            messages.error(request, 'Invalid request, no lessons have been subscribed or unsubscribed to.')
+            messages.error(request, 'Invalid request, no lessons have been \
+                                    subscribed or unsubscribed to.')
             return redirect(reverse('lessons'))
 
 
@@ -176,11 +190,12 @@ def instructor_created_lessons(request):
     if not profile.is_instructor:
         messages.error(request, 'Only instructors can do this.')
         return redirect('home')
-    
+
     template = 'lessons/instructor_created_lessons.html'
 
     # Get lesson items bound to student
-    instructor_created_lessons = Lesson.objects.filter(instructor_profile=profile)
+    instructor_created_lessons = Lesson.objects.filter(
+        instructor_profile=profile)
 
     context = {
         'profile': profile,
@@ -208,7 +223,9 @@ def delete_instructor_created_lesson(request, id):
         instructor_created_lesson.delete()
         return redirect('instructor_created_lessons')
     else:
-        messages.error(request, 'This lesson does not belong to you and has not been deleted, please check your username and try again.')
+        messages.error(request, 'This lesson does not belong to you and has \
+                                not been deleted, please check your username \
+                                and try again.')
         return redirect(reverse('instructor_created_lessons'))
 
 
@@ -225,13 +242,14 @@ def create_lesson(request):
         lesson_name = request.POST.get('lesson_name')
 
         # Check for duplicate names
-        instructor_created_lessons = Lesson.objects.filter(instructor_profile=profile).values_list('lesson_name', flat=True)
+        instructor_created_lessons = Lesson.objects.filter(
+            instructor_profile=profile).values_list('lesson_name', flat=True)
 
         if lesson_name not in instructor_created_lessons:
             # Create lesson
             form = LessonForm(request.POST, request.FILES)
             if form.is_valid():
-                lesson = form.save(commit=False)  # Delay commit of form to enter profile on next line
+                lesson = form.save(commit=False)
                 lesson.instructor_profile = profile
                 lesson.save()
                 return redirect('instructor_created_lessons')
@@ -241,7 +259,7 @@ def create_lesson(request):
             return redirect(reverse('instructor_created_lessons'))
 
     else:
-        form = LessonForm(initial={'instructor_profile':profile})  # Insert current user in this field
+        form = LessonForm(initial={'instructor_profile': profile})
         template = 'lessons/create_lesson.html'
         context = {
             'form': form
@@ -262,9 +280,11 @@ def edit_lesson(request, lesson_id):
     except Exception as e:
         messages.error(request, 'Invalid lesson ID, no lessons were updated.')
         return redirect(reverse('instructor_created_lessons'))
-    
+
     if request.method == 'POST':
-        form = LessonForm(request.POST, request.FILES, instance=instructor_lesson)
+        form = LessonForm(request.POST,
+                          request.FILES,
+                          instance=instructor_lesson)
         if form.is_valid():
             form.save()
         return redirect('instructor_created_lessons')
@@ -281,7 +301,8 @@ def edit_lesson(request, lesson_id):
             }
             return render(request, template, context)
         else:
-            messages.error(request, 'You can only edit your own lessons, please check your username.')
+            messages.error(request, 'You can only edit your own lessons, \
+                                     please check your username.')
             return redirect(reverse('instructor_created_lessons'))
 
 
@@ -293,14 +314,17 @@ def review_lesson(request, lesson_id):
     try:
         lesson = get_object_or_404(Lesson, lesson_id=lesson_id)
     except Exception as e:
-        messages.error(request, 'Cannot create/edit a review for an invalid lesson.')
+        messages.error(request, 'Cannot create/edit a review for \
+                                 an invalid lesson.')
         return redirect(reverse('home'))
 
-    existing_review = LessonReview.objects.filter(profile=profile, lesson=lesson).first()
+    existing_review = LessonReview.objects.filter(profile=profile,
+                                                  lesson=lesson).first()
     # Check existing review belongs to current users profile
     if existing_review:
         if not existing_review.profile == profile:
-            messages.error(request, 'Cannot complete request, this review is not yours.')
+            messages.error(request, 'Cannot complete request, this \
+                                     review is not yours.')
             return redirect(reverse('home'))
 
     template = "lessons/create_review.html"
@@ -314,7 +338,8 @@ def review_lesson(request, lesson_id):
         # Validate rating
         rating_value = request.POST['rating_dropdown']
         if int(rating_value) not in range(1, 11):
-            messages.error(request, 'You entered an invalid rating, please try again.')
+            messages.error(request, 'You entered an invalid rating, \
+                                     please try again.')
             return redirect('studio', lesson.lesson_id)
 
         rating_value = int(rating_value)
@@ -340,6 +365,6 @@ def review_lesson(request, lesson_id):
             context['form'] = form
             return render(request, template, context)
         else:
-            form = ReviewForm(initial={'profile': profile, 'lesson': lesson})  # Insert current user in this field
+            form = ReviewForm(initial={'profile': profile, 'lesson': lesson})
             context['form'] = form
             return render(request, template, context)
