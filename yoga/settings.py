787ec811b27f2598c4063e5ee2cf4986917d9542
Kelvin <kwardeu@gmail.com>
2021-02-03 13:03:33 +0000

    BUGFIX: settings.py error setting up enviromental variables

--- yoga/settings.py
+++ yoga/settings.py
@@ -235,6 +235,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.envron.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.envron.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.envron.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
