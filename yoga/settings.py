2917ff664bea0eef8473831845c3ce832f11b03d
Kelvin <kwardeu@gmail.com>
2021-02-03 13:00:29 +0000

    FEAT: Add email functionality

--- yoga/settings.py
+++ yoga/settings.py
@@ -110,8 +110,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-# Setup backend email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email/allauth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -226,4 +225,16 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_CURRENCY = 'eur'
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
-DEFAULT_FROM_EMAIL = 'info@socialyoga.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@socialyoga.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.envron.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.envron.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.envron.get('EMAIL_HOST_USER')
