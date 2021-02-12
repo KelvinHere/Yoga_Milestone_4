dc595fd6b20eefd0350c89dd064c6f18c859f404
Kelvin <kwardeu@gmail.com>
2021-02-12 10:23:05 +0000

    TEST: Add tests for instructor_admin and subscriptions

--- lessons/tests/test_views.py
+++ lessons/tests/test_views.py
@@ -15,11 +15,18 @@ class TestLessonViews(TestCase):
         self.free_lesson = Lesson.objects.filter(is_free=True).first()
         self.paid_lesson = Lesson.objects.filter(is_free=False, lesson_name='Z Lesson').first()
         self.invalid_lesson_id = "SDFGGRFVAD"
+        self.subscribed_user = UserProfile.objects.get(id=5)  # = incomplete_user
+        self.subscribed_lesson = Lesson.objects.get(id=2)
+        self.subscription = Subscription.objects.get(lesson=self.subscribed_lesson,
+                                                     user=self.subscribed_user)
         self.complete_user = {'username': 'complete_user',
                               'password': 'orange99'}
         self.incomplete_user = {'username': 'incomplete_user',
                                 'password': 'orange99'}
+        self.instructor_credentials = {'username': 'instructor_2',
+                                       'password': 'orange99'}
 
+    # Lessons View
     def test_get_lessons(self):
         # Renders a list of all lessons
         response = self.client.get('/lessons/')
@@ -27,6 +34,7 @@ class TestLessonViews(TestCase):
         self.assertTemplateUsed(response, 'lessons/lessons.html')
         self.assertContains(response, 'All Lessons')
 
+    # Lesson Page with instructor filter
     def test_lessons_with_valid_instructor_filter(self):
         # Displays the instructors profile and all their
         # lessons underneath
@@ -36,6 +44,7 @@ class TestLessonViews(TestCase):
         self.assertContains(response, self.instructor.user.username)
         self.assertContains(response, html.escape(self.instructor.profile_description))
 
+    # Subscription View
     def test_subscriptions_logged_out(self):
         # Logged out users will be redirect to login page
         response = self.client.get(f'/lessons/subscriptions/', follow=True)
@@ -106,4 +115,44 @@ class TestLessonViews(TestCase):
             response,
             'Invalid request, no lessons have been subscribed')
 
-    # Make test to create subscription then remove it with subscribe=false
+    def test_unsubscribe_to_lesson_valid_request(self):
+        # Valid request will remove the subscription
+        # object for this user / lesson
+        login_successful = self.client.login(username=self.incomplete_user['username'],
+                                             password=self.incomplete_user['password'])
+        self.assertTrue(login_successful)
+        response = self.client.get('/lessons/subscriptions/', {'subscribe': 'false', 'lesson_id': self.subscribed_lesson.lesson_id}, follow=True)
+        self.assertFalse(Subscription.objects.filter(lesson=self.subscribed_lesson, user=self.subscribed_user).exists())
+
+    # Instructor_admin View
+    def test_instructor_admin_logged_out(self):
+        # Logged out users will be redirect to login page
+        response = self.client.get(f'/lessons/instructor_admin/', follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertRedirects(response, f'/accounts/login/?next=/lessons/instructor_admin/')
+        self.assertContains(response, html.escape('If you have not created an account yet, then please'))
+
+
+    def test_instructor_admin_not_instructor(self):
+        # A non instructor will be redirected home with an error message
+        login_successful = self.client.login(username=self.complete_user['username'],
+                                             password=self.complete_user['password'])
+        self.assertTrue(login_successful)
+        response = self.client.get(f'/lessons/instructor_admin/', follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertContains(response, 'Only instructors can do this.')
+
+    def test_instructor_admin_as_instructor(self):
+        # Instructors can view this page and see lessons / sales
+        login_successful = self.client.login(username=self.instructor_credentials['username'],
+                                             password=self.instructor_credentials['password'])
+        self.assertTrue(login_successful)
+        response = self.client.get(f'/lessons/instructor_admin/', follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lessons/instructor_admin.html')
+        self.assertContains(response, 'Lesson Admin for instructor_2')
+        # Contains instructors lessons
+        self.assertContains(response, html.escape("Instructor 2's first lesson"))
+        # Contains instructors sales
+        self.assertContains(response, html.escape("24.99 - 30%"))
