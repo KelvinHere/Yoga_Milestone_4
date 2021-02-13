08058a7f8ea7199fd1582e63f54bde6586ad9ceb
Kelvin <kwardeu@gmail.com>
2021-02-13 15:53:36 +0000

    TEST: Add basket tests that check discount figures and banners react accordingly

--- basket/tests/test_views.py
+++ basket/tests/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.conf import settings
 
 import html
 
+from basket.contexts import basket_contents
 from profiles.models import UserProfile
 from lessons.models import Subscription, Lesson
 from checkout.models import OrderLineItem
@@ -15,6 +17,72 @@ class TestBasketViews(TestCase):
         login_successful = self.client.login(username=profile.user.username,
                                              password='orange99')
         self.assertTrue(login_successful)
+
+    def test_discount_met(self):
+        '''
+        Discount is  met and deducted from grand total
+        banner and total shows discount is applied
+        and discount is correctly applied
+        '''
+        # Remove all bought items
+        OrderLineItem.objects.all().delete()
+
+        # Check lesson is over discount threshold
+        lesson = Lesson.objects.get(lesson_name='B Lesson')
+        self.assertGreaterEqual(lesson.price, settings.DISCOUNT_THRESHOLD)
+
+        # Add item to basket
+        response = self.client.post('/basket/add_to_basket/', {'lesson_id': lesson.lesson_id}, follow=True)
+        self.assertContains(response, '"item_added": "True"')
+        response = self.client.get('/basket/', follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'basket/basket.html')
+
+        # Send request through context processor to get current grand total
+        request = response.wsgi_request
+        grand_total = round(basket_contents(request)['grand_total'], 2)
+        # Calculate grand total
+        test_grand_total = round(lesson.price - ((lesson.price / 100) * settings.DISCOUNT_PERCENTAGE), 2)
+        # Do totals match
+        self.assertEqual(grand_total, test_grand_total)
+
+        # Lesson details and price appear on webpage
+        self.assertContains(response, 'B Lesson')
+        self.assertContains(response, html.escape(f'Total: €{lesson.price}'))
+        self.assertContains(response, html.escape(f'Grand Total: €{grand_total}'))
+        self.assertContains(response, f'{settings.DISCOUNT_PERCENTAGE}% discount applied!')
+        
+        # Discount banner threshold met
+        self.assertContains(response, f'A {settings.DISCOUNT_PERCENTAGE}% discount has been applied to your basket!')
+
+    def test_discount_not_met(self):
+        '''
+        Discount is not met and not deducted from grand total
+        amount more to spend for discount is displayed
+        '''
+        OrderLineItem.objects.all().delete()
+        # Check lesson is under discount threshold
+        lesson = Lesson.objects.get(lesson_name='Z Lesson')
+        self.assertLess(lesson.price, settings.DISCOUNT_THRESHOLD)
+
+        # Add item to basket
+        response = self.client.post('/basket/add_to_basket/', {'lesson_id': lesson.lesson_id}, follow=True)
+        self.assertContains(response, '"item_added": "True"')
+        response = self.client.get('/basket/', follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'basket/basket.html')
+        
+        # Lesson details and price
+        self.assertContains(response, 'Z Lesson')
+        self.assertContains(response, html.escape(f'Total: €{lesson.price}'))
+        self.assertContains(response, html.escape(f'Grand Total: €{lesson.price}'))
+        
+        # Discount delta
+        discount_delta = settings.DISCOUNT_THRESHOLD - lesson.price
+        self.assertContains(response, html.escape(f'Spend €{discount_delta} more for a {settings.DISCOUNT_PERCENTAGE}% discount!'))
+        
+        # Discount percentage
+        self.assertContains(response, html.escape(f'Get a {settings.DISCOUNT_PERCENTAGE}% discount when you spend more than'))
 
     def test_logged_out(self):
         ''' Logged out users will be redirect to login page '''
@@ -98,48 +166,8 @@ class TestBasketViews(TestCase):
         # Only contains one of this item in basket
         self.assertEqual(html_str.count(f'id="row_for_{lesson.lesson_id}'), 1)
 
-    def test_discount_not_met(self):
+    def test_remove_from_basket_valid_request(self):
         '''
-        Discount is not met and not deducted from grand total
-        amount more to spend for discount is displayed
+        Removes an item from the basket
         '''
-        basket = self.client.session['basket']
-        
-
-        OrderLineItem.objects.all().delete()
-        lesson = Lesson.objects.get(lesson_name='Z Lesson')
-        response = self.client.post('/basket/add_to_basket/', {'lesson_id': lesson.lesson_id}, follow=True)
-        self.assertContains(response, '"item_added": "True"')
-        
-        response = self.client.get('/basket/', follow=True)
-        self.assertTrue(response.status_code, 200)
-        self.assertTemplateUsed(response, 'basket/basket.html')
-        self.assertContains(response, 'Z Lesson')
-        self.assertContains(response, html.escape(f'Total: €{lesson.price}'))
-        self.assertContains(response, html.escape(f'Grand Total: €{lesson.price}'))
-        self.assertContains(response, html.escape('more for a 10% discount!'))
-        # Discount banner, threshold not met
-        self.assertContains(response, html.escape('Get a 10% discount when you spend more than'))
-
-    def test_discount_met(self):
-        '''
-        Discount is  met and deducted from grand total
-        banner and total shows discount is applied
-        '''
-        OrderLineItem.objects.all().delete()
-        lesson = Lesson.objects.get(lesson_name='B Lesson')
-        response = self.client.post('/basket/add_to_basket/', {'lesson_id': lesson.lesson_id}, follow=True)
-        self.assertContains(response, '"item_added": "True"')
-
-        response = self.client.get('/basket/', follow=True)
-        self.assertTrue(response.status_code, 200)
-        self.assertTemplateUsed(response, 'basket/basket.html')
-        print(response.content.decode("utf-8"))
-        self.assertContains(response, 'B Lesson')
-        self.assertContains(response, html.escape(f'Total: €{lesson.price}'))
-        self.assertLess(response, '))
-        self.assertContains(response, 'discount applied!')
-        # Discount banner threshold met
-        self.assertContains(response, 'A 10% discount has been applied to your basket!')
-
 
