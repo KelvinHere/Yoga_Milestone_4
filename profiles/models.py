2cec6c643e9520a3f80d3caf049309c3a5cc5e2f
Kelvin <kwardeu@gmail.com>
2021-02-17 19:51:48 +0000

    BUGFIX: Userprofile creation

--- profiles/models.py
+++ profiles/models.py
@@ -63,7 +63,7 @@ class UserProfile(models.Model):
         self.save()
 
 
-#@receiver(post_save, sender=User)
+@receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile through signals/reciever
