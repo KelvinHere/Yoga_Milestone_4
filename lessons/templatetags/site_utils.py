5891f08d9e2083458ab22e4228d4e6361dc3a4ad
Kelvin <kwardeu@gmail.com>
2021-02-20 20:05:36 +0000

    BUGFIX: instructor_admin sales page, sales percentage is current sales percentage, not the percentage at time of sale

--- lessons/templatetags/site_utils.py
+++ lessons/templatetags/site_utils.py
@@ -5,9 +5,9 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.filter(name='deduct_sales_percentage')
-def deduct_sales_percentage(value):
-    site_percentage = settings.SITE_SALES_PERCENTAGE,
+@register.filter(name='deduct_lineitem_sales_percentage')
+def deduct__lineitem_sales_percentage(value, percentage):
+    site_percentage = percentage,
     discount = value * Decimal(site_percentage[0]) / 100
     value -= discount
     return value
